f6c127685154db36ddd6278772023c891d4e3ca5
blappdeveloper <leeladher.aj.63@gmail.com>
2021-05-21 11:48:36 +0530

    update

--- app/src/main/java/com/fortrade/tiktok/MainActivity.kt
+++ app/src/main/java/com/fortrade/tiktok/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.fortrade.tiktok
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.fortrade.tiktok.authentication.PhoneAuthActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser==null){
+            startActivity(Intent(this,PhoneAuthActivity::class.java))
+        }else{
+            Toast.makeText(this,"Already Sigh In",Toast.LENGTH_SHORT).show()
+        }
     }
 }
