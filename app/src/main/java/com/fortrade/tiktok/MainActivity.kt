eaadae65d495047f3faa0da43cc3ed01bb364661
PumpkinDev <aryeahtyagi@gmail.com>
2021-05-29 17:12:30 +0530

    Added : Nav controller to navigate b/w fragments

--- app/src/main/java/com/fortrade/tiktok/MainActivity.kt
+++ app/src/main/java/com/fortrade/tiktok/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.navigation.findNavController
 import com.fortrade.tiktok.authentication.AuthFragment
 import com.google.firebase.auth.FirebaseAuth
 
@@ -15,19 +16,14 @@ class MainActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
     }
 
+
     override fun onStart() {
         super.onStart()
+
         if (auth.currentUser==null){
-           val authFragment = AuthFragment()
-            val fragmentManager  = supportFragmentManager
-            val transaction = fragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment,authFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-
-
+            // then we will navigate somewhere else
         }else{
-            Toast.makeText(this,"Already Sigh In",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,R.string.signed_in,Toast.LENGTH_SHORT).show()
         }
     }
 }
