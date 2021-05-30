4854eee08ec8d3d02e8d725c694a2c19f10fd2d5
leeladher <leeladher.aj.63@gmail.com>
2021-05-30 16:19:21 +0530

    update

--- app/src/main/java/com/fortrade/tiktok/authentication/AuthFragment.kt
+++ app/src/main/java/com/fortrade/tiktok/authentication/AuthFragment.kt
@@ -3,6 +3,7 @@ package com.fortrade.tiktok.authentication
 import android.app.Activity
 import android.app.ProgressDialog
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,7 +12,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.fortrade.tiktok.R
 import com.fortrade.tiktok.databinding.FragmentAuthBinding
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
@@ -24,11 +24,14 @@ class AuthFragment : Fragment() {
 
     private var _binding: FragmentAuthBinding? = null
     private val binding get() = _binding!!
+    var disable: Boolean = false
+    lateinit var conCode : String
+    lateinit var phone: String
+    lateinit var fullNumber: String
+    private var forceResendingToken: PhoneAuthProvider.ForceResendingToken? = null
 
-    private var forceResendingToken: PhoneAuthProvider.ForceResendingToken?=null
-
-    private var mCollBacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks?=null
-    private var mVerificationId:String?=null
+    private var mCollBacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks? = null
+    private var mVerificationId: String? = null
     private lateinit var firebaseAuth: FirebaseAuth
 
     private val TAG = "TAGES"
@@ -47,7 +50,7 @@ class AuthFragment : Fragment() {
 
         binding.scrollViewPhoneAuth.visibility = View.VISIBLE
         binding.ScrollViewOTP.visibility = View.GONE
-
+        val timer = MyCounter(60000, 1000)
         firebaseAuth = FirebaseAuth.getInstance()
 
         progressDialog = ProgressDialog(context)
@@ -65,10 +68,13 @@ class AuthFragment : Fragment() {
                 progressDialog.dismiss()
                 binding.phoneBox.error = "Enter Valid Mobile Number"
                 binding.phoneBox.requestFocus()
-                Log.d("phones","${e.message}")
+                Log.d("phoness", "${e.message}")
             }
 
-            override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
                 Log.d(TAG, "onCodeSend : $verificationId")
                 mVerificationId = verificationId
                 forceResendingToken = token
@@ -79,118 +85,138 @@ class AuthFragment : Fragment() {
                 binding.ScrollViewOTP.visibility = View.VISIBLE
 
                 Toast.makeText(context, "Verification Code Send", Toast.LENGTH_SHORT).show()
-                binding.textView5.text ="+91${binding.phoneBox.text}"
-            }
-        }
-        //Phone Get Otp
-        binding.otpBtn.setOnClickListener {
-            val conCode = binding.conCode.text.toString()
-            val phone = binding.phoneBox.text.toString()
-            val fullNumber = "$conCode$phone"
-            Log.d("fullNumber","full number: $fullNumber")
-            Log.d("phone","number: $phone")
-            //validate phone number
-            if (TextUtils.isEmpty(fullNumber)){
-                Toast.makeText(context, "Please Enter Phone Number ", Toast.LENGTH_SHORT).show()
-
-            }else{
-                startPhoneNumberVerification(fullNumber)
+                binding.textView5.text = "+91${binding.phoneBox.text}"
+                timer.start()
             }
         }
 
-        //Resend Code
-        binding.resend.setOnClickListener {
-            val conCode = binding.conCode.text.toString()
-            val phone = binding.phoneBox.text.toString()
-            val fullNumber = "$conCode$phone"
-            Log.d("fullNumber","full number: $fullNumber")
-            Log.d("phone","full number: $phone")
-            //validate phone number
-            if (TextUtils.isEmpty(fullNumber)){
-                Toast.makeText(context, "Please Enter Phone Number ", Toast.LENGTH_SHORT).show()
+                //Phone Get Otp
+                binding.otpBtn.setOnClickListener {
+                   conCode = binding.conCode.text.toString()
+                   phone = binding.phoneBox.text.toString()
+                   fullNumber = "$conCode$phone"
+                    Log.d("fullNumber", "full number: $fullNumber")
+                    Log.d("phone", "number: $phone")
+                    //validate phone number
+                    if (TextUtils.isEmpty(fullNumber)) {
+                        Toast.makeText(context, "Please Enter Phone Number ", Toast.LENGTH_SHORT)
+                            .show()
 
-            }else{
-                resendVerificationCode(fullNumber,forceResendingToken)
+                    } else {
+                        startPhoneNumberVerification(fullNumber)
+                    }
+                }
+
+                //Resend Code
+                binding.resend.setOnClickListener {
+
+                    if (disable == true) {
+//                        val conCode = binding.conCode.text.toString()
+//                        val phone = binding.phoneBox.text.toString()
+//                      fullNumber = "$conCode$phone"
+                        Log.d("fullNumber", "full number: $fullNumber")
+                        Log.d("phone", "full number: $phone")
+                        //validate phone number
+
+                       resendVerificationCode(fullNumber, forceResendingToken)
+                        disable = false
+                    }
+                }
+
+                //Verify
+                binding.verifyBtn.setOnClickListener {
+                    val code = binding.otpEdt.text.toString().trim()
+                    if (TextUtils.isEmpty(code)) {
+                        Toast.makeText(context, "Please Enter OTP", Toast.LENGTH_SHORT).show()
+
+                    } else {
+                        verifyPhoneNumberWithCode(mVerificationId, code)
+                    }
+                }
             }
-        }
-
-        //Verify
-        binding.verifyBtn.setOnClickListener {
-            val code = binding.otpEdt.text.toString().trim()
-            if (TextUtils.isEmpty(code)){
-                Toast.makeText(context, "Please Enter OTP", Toast.LENGTH_SHORT).show()
-
-            }else{
-                verifyPhoneNumberWithCode(mVerificationId,code)
-            }
-        }
-    }
-
-
-
 
 
 //functions
 
-private fun startPhoneNumberVerification(fullNumber:String){
-    progressDialog.setMessage("Verifying Phone Number...")
-    progressDialog.show()
+            private fun startPhoneNumberVerification(fullNumber: String) {
+                progressDialog.setMessage("Verifying Phone Number...")
+                progressDialog.show()
 
 
-    val options= PhoneAuthOptions.newBuilder(firebaseAuth)
-        .setPhoneNumber(fullNumber)
-        .setTimeout(60L, TimeUnit.SECONDS)
-        .setActivity(context as Activity)
-        .setCallbacks(mCollBacks)
-        .build()
-    PhoneAuthProvider.verifyPhoneNumber(options)
-}
+                val options = PhoneAuthOptions.newBuilder(firebaseAuth)
+                    .setPhoneNumber(fullNumber)
+                    .setTimeout(60L, TimeUnit.SECONDS)
+                    .setActivity(context as Activity)
+                    .setCallbacks(mCollBacks)
+                    .build()
+                PhoneAuthProvider.verifyPhoneNumber(options)
+            }
 
-private fun resendVerificationCode(fullNumber: String,token: PhoneAuthProvider.ForceResendingToken?){
-    progressDialog.setMessage("Resending Code...")
-    progressDialog.show()
+            private fun resendVerificationCode(
+                fullNumber: String,
+                token: PhoneAuthProvider.ForceResendingToken?
+            ) {
+                progressDialog.setMessage("Resending Code...")
+                progressDialog.show()
 
 
-    val options= PhoneAuthOptions.newBuilder(firebaseAuth)
-        .setPhoneNumber(fullNumber)
-        .setTimeout(60L, TimeUnit.SECONDS)
-        .setActivity(context as Activity)
-        .setCallbacks(mCollBacks)
-        .setForceResendingToken(token)
-        .build()
-    PhoneAuthProvider.verifyPhoneNumber(options)
-}
+                val options = PhoneAuthOptions.newBuilder(firebaseAuth)
+                    .setPhoneNumber(fullNumber)
+                    .setTimeout(60L, TimeUnit.SECONDS)
+                    .setActivity(context as Activity)
+                    .setCallbacks(mCollBacks)
+                    .setForceResendingToken(token)
+                    .build()
+                PhoneAuthProvider.verifyPhoneNumber(options)
+            }
 
-private fun verifyPhoneNumberWithCode(verificationId:String?,code:String){
-    progressDialog.setMessage("Verifying Code...")
-    progressDialog.show()
+            private fun verifyPhoneNumberWithCode(verificationId: String?, code: String) {
+                progressDialog.setMessage("Verifying Code...")
+                progressDialog.show()
 
-    val credential = PhoneAuthProvider.getCredential(verificationId,code)
-    signInWithPhoneAuthCredential(credential)
+                val credential = PhoneAuthProvider.getCredential(verificationId, code)
+                signInWithPhoneAuthCredential(credential)
 
-}
-private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
-    progressDialog.setMessage("Logging In...")
+            }
 
-    firebaseAuth .signInWithCredential(credential)
-        .addOnSuccessListener {
-            progressDialog.dismiss()
-            val phone = firebaseAuth.currentUser.phoneNumber
-            Toast.makeText(context, "Loggin with as $phone", Toast.LENGTH_SHORT).show()
+            private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+                progressDialog.setMessage("Logging In...")
 
-            findNavController().navigate(R.id.action_authFragment_to_updateProfileFragment)
+                firebaseAuth.signInWithCredential(credential)
+                    .addOnSuccessListener {
+                        progressDialog.dismiss()
+                        val phone = firebaseAuth.currentUser.phoneNumber
+                        Toast.makeText(context, "Loggin with as $phone", Toast.LENGTH_SHORT).show()
 
+                        findNavController().navigate(R.id.action_authFragment_to_updateProfileFragment)
+
+                    }
+                    .addOnFailureListener { e ->
+                        progressDialog.dismiss()
+                        binding.otpEdt.error = "Enter Valid OTP"
+                        binding.otpEdt.requestFocus()
+                    }
+            }
+
+            inner class MyCounter(millisInFuture: Long, countDownInterval: Long) :
+                CountDownTimer(millisInFuture, countDownInterval) {
+
+                override fun onFinish() {
+                    println("Timer Completed.")
+                    disable = true
+                    binding.resend.text = "Resend"
+                }
+
+                override fun onTick(millisUntilFinished: Long) {
+
+                    binding.resend.text = (millisUntilFinished / 1000).toString() + ""
+                    println("Timer  : " + millisUntilFinished / 1000)
+                }
+            }
+
+
+            override fun onDestroyView() {
+                super.onDestroyView()
+                _binding = null
+            }
         }
-        .addOnFailureListener{e->
-            progressDialog.dismiss()
-            binding.phoneBox.error = "Enter Valid OTP"
-            binding.phoneBox.requestFocus()
-        }
-}
-
-
-     override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
