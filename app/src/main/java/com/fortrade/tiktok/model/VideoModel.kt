ce8cd4a0890272cf71a6db2e93b9572c7e8e7263
leeladher <leeladher.aj.63@gmail.com>
2021-06-24 15:05:00 +0530

    uploadVideo

--- app/src/main/java/com/fortrade/tiktok/model/VideoModel.kt
+++ app/src/main/java/com/fortrade/tiktok/model/VideoModel.kt
@@ -1,6 +1,8 @@
 package com.leeladher.video
 
 data class VideoModel(
-    var videoUrl: String = "", var likes:String = "",var uploaderKey:String = ""
-    ,var uniqueVideoId:String = ""
+    var videoUrl: String = "",
+    var likes: String = "",
+    var uploaderKey: String = "",
+    var uniqueVideoId: String = ""
 )
